b7257af076d8846db811a474dd377a423dec26d1
theevity1 <theevity1@gmail.com>
2021-09-02 19:44:37 -0300

    skipdialogue
    yes

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -31,6 +31,8 @@ class DialogueBox extends FlxSpriteGroup
 	var portraitLeft:FlxSprite;
 	var portraitRight:FlxSprite;
 
+	var SkipThisShit:FlxText;
+
 	var handSelect:FlxSprite;
 	var bgFade:FlxSprite;
 
@@ -128,6 +130,13 @@ class DialogueBox extends FlxSpriteGroup
 		box.updateHitbox();
 		add(box);
 
+		if (hasDialog)
+			{
+				SkipThisShit = new FlxText(0, FlxG.height * 0.92, -100, "Press SPACE to skip", 32);
+				SkipThisShit.font = 'Crewniverse';
+				add(SkipThisShit);
+			}
+
 		box.screenCenter(X);
 		portraitLeft.screenCenter(X);
 
@@ -182,6 +191,34 @@ class DialogueBox extends FlxSpriteGroup
 			}
 		}
 
+		if (FlxG.keys.justPressed.SPACE && dialogueStarted == true)
+			{
+				if (!isEnding)
+				{
+					remove(dialogue);
+					remove(SkipThisShit);
+					isEnding = true;
+		
+					new FlxTimer().start(0.2, function(tmr:FlxTimer)
+					{
+						box.alpha -= 1 / 5;
+						bgFade.alpha -= 1 / 5 * 0.7;
+						portraitLeft.visible = false;
+						portraitRight.visible = false;
+						swagDialogue.alpha -= 1 / 5;
+						dropText.alpha = swagDialogue.alpha;
+					}, 5);
+		
+					new FlxTimer().start(1.2, function(tmr:FlxTimer)
+					{
+						finishThing();
+						kill();
+					});
+					
+					super.update(elapsed);
+				}
+			}
+
 		if (dialogueOpened && !dialogueStarted)
 		{
 			startDialogue();
@@ -199,6 +236,7 @@ class DialogueBox extends FlxSpriteGroup
 				if (!isEnding)
 				{
 					isEnding = true;
+					remove(SkipThisShit);
 
 					if (PlayState.SONG.song.toLowerCase() == 'senpai' || PlayState.SONG.song.toLowerCase() == 'thorns')
 						FlxG.sound.music.fadeOut(2.2, 0);
